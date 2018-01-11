41394dd1a99743fe2bed0b562a42dbf2d35ac657
srvstva <ankursrivastava35@gmail.com>
2018-01-11 15:55:28 +0530

    Fixed bug finding release from JSON response

--- templates/bin/suite_db_uploader.py
+++ templates/bin/suite_db_uploader.py
@@ -4,15 +4,15 @@ import argparse
 
 
 def get_release_id(release):
-    """Gets the release id corresponding to the 
+    """Gets the release id corresponding to the
     release name from the api endpoint
-    if the corresponding release is not available 
+    if the corresponding release is not available
     it makes a post request to the endpoint
     and inserts into the database.
     Returns the newly created id"""
-    api_endpoint = API_ENDPOINT + 'release' 
-    r = session.get(api_endpoint)
-    json_resp = r.json() 
+    api_endpoint = API_ENDPOINT + 'release'
+    r = session.get(api_endpoint, params=dict(results_per_page=1000))
+    json_resp = r.json()
     if not 'objects' in json_resp:
         pass
 
@@ -20,20 +20,20 @@ def get_release_id(release):
     if release not in releases:
         # Need to update in database
         r = session.post(api_endpoint, json=dict(name=release))
-        resp = r.json() 
+        resp = r.json()
         releaseid = resp['id']
         try:
-            r.raise_for_status() 
+            r.raise_for_status()
         except requests.HTTPError as e:
             print 'Unable to POST to endpoint'
             print e.message
-            exit() 
+            exit()
     else:
         for o in json_resp['objects']:
             if o.get('name') == release:
-                releaseid = o.get('id') 
+                releaseid = o.get('id')
     return releaseid
-     
+
 
 def populate_db_results(result_file):
     """Creates a list of results.
@@ -42,15 +42,15 @@ def populate_db_results(result_file):
     db_results = []
     with open(result_file) as f:
         for line in f:
-            if line.startswith("#"): 
-                continue 
+            if line.startswith("#"):
+                continue
             name, testrun, componentid, categoryid, status, desc = line.strip().split(',')
             row = dict(
-                name=name, 
-                testrun=testrun, 
-                categoryid=categoryid, 
-                componentid=componentid, 
-                status=status, 
+                name=name,
+                testrun=testrun,
+                categoryid=categoryid,
+                componentid=componentid,
+                status=status,
                 desc=desc
             )
             db_results.append(row)
@@ -72,58 +72,58 @@ def upload(releaseid, buildid, db_results):
             componentid=result['componentid'],
             status=result['status']
         )
-        
+
         r = session.post(API_ENDPOINT + 'testcase', json=json_payload)
         try:
             r.raise_for_status()
         except requests.HTTPError as e:
             print e.message
-        
+
         count += 1
-    return count 
+    return count
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser() 
+    parser = argparse.ArgumentParser()
     parser.add_argument(
-        '-r', 
-        '--release', 
-        required=True, 
+        '-r',
+        '--release',
+        required=True,
         help='Release name'
-    ) 
+    )
     parser.add_argument(
-        '-v', 
-        '--version', 
-        required=True, 
+        '-v',
+        '--version',
+        required=True,
         help='The build number'
-    ) 
+    )
     parser.add_argument(
-        '-f', 
-        '--file', 
-        required=True, 
+        '-f',
+        '--file',
+        required=True,
         help='The result file'
     )
     parser.add_argument(
-        '-e', 
-        '--endpoint', 
-        help='API Endpoint(default=10.10.30.37/api)', 
+        '-e',
+        '--endpoint',
+        help='API Endpoint(default=10.10.30.37/api)',
         default='http://10.10.30.37/api/'
     )
     opts = parser.parse_args()
-    
-    # Create a session to be used by each 
-    # request. 
+
+    # Create a session to be used by each
+    # request.
     # Session allows the connection to be reused
     # thus making it lot faster
-    session = requests.session() 
-    API_ENDPOINT = opts.endpoint 
+    session = requests.session()
+    API_ENDPOINT = opts.endpoint
 
     releaseid = get_release_id(opts.release)
     db_results = populate_db_results(opts.file)
     count = upload(
-        releaseid, 
-        opts.version, 
+        releaseid,
+        opts.version,
         db_results
-    ) 
+    )
     print '%d rows uploaded' % count
 
